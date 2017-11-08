8c441f62b797512618df6c407013e75440b6a3d1
Landtree <delandtree@gmail.com>
2017-11-08 13:40:46 -0800

    updated into a function
    define script as a function with a return value assigned to port.

--- USB_Dectection.py
+++ USB_Dectection.py
@@ -12,43 +12,51 @@ import subprocess
 from subprocess import Popen
 import serial
 
-#create search IDs
-Mega = "0042"
-Uno = "0043"
-Red = "6015"
-Leo = "0005"
+def find_port():
+    #create search IDs
+    Mega = "0042"
+    Uno = "0043"
+    Red = "6015"
+    Leo = "0005"
 
-#look for ports attached
-tmp = os.popen('python' ' -m' ' serial.tools.list_ports' ' -v').read()
+    #look for ports attached
+    tmp = os.popen('python' ' -m' ' serial.tools.list_ports' ' -v').read()
 
-#serach for typer of Arduino and captures its port
-if Mega in tmp[:]:
-    portUSB = tmp.split('\n', 1)
-    print "Device is a Mega"
-    print "It is attached to: " + portUSB[0]
-    port = str(portUSB[0])
-    port = port.strip()
+    #serach for typer of Arduino and captures its port
+    if Mega in tmp[:]:
+        portUSB = tmp.split('\n', 1)
+        print "Device is a Mega"
+        print "It is attached to: " + portUSB[0]
+        port = str(portUSB[0])
+        port = port.strip()
+        return port
 
-if Uno in tmp[:]:
-    portUSB = tmp.split('\n', 1)
-    print "Device is an Uno"
-    print "It is attached to: " + portUSB[0]
-    port = str(portUSB[0])
-    port = port.strip()
+    if Uno in tmp[:]:
+        portUSB = tmp.split('\n', 1)
+        print "Device is an Uno"
+        print "It is attached to: " + portUSB[0]
+        port = str(portUSB[0])
+        port = port.strip()
+        return port
 
-if Red in tmp[:]:
-    portUSB = tmp.split('\n', 1)
-    print "Device is a Redboard"
-    print "It is attached to:"
-    print "It is attached to: " + portUSB[0]
-    port = port.strip()
+    if Red in tmp[:]:
+        portUSB = tmp.split('\n', 1)
+        print "Device is a Redboard"
+        print "It is attached to:"
+        print "It is attached to: " + portUSB[0]
+        port = port.strip()
+        return port
 
-if Leo in tmp[:]:
-    portUSB = tmp.split('\n', 1)
-    print "Device is a Leonardo"
-    print "It is attached to:"
-    print "It is attached to: " + portUSB[0]
-    port = port.strip()
+    if Leo in tmp[:]:
+        portUSB = tmp.split('\n', 1)
+        print "Device is a Leonardo"
+        print "It is attached to:"
+        print "It is attached to: " + portUSB[0]
+        port = port.strip()
+        return port
 
+
+#assigns port from function
+port = str(find_port())
 #attachs Arduino to ser
 ser = serial.Serial(port, 115200, timeout = 1)
