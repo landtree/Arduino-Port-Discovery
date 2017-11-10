e3cafb9649f74499b49ca845784d746b1481f0ba
Landtree <delandtree@gmail.com>
2017-11-09 16:25:31 -0800

    added FTDI
    Added FTDI cable & FTDI chips (such as sparkFun FTDI Basic)

--- USB_Dectection.py
+++ USB_Dectection.py
@@ -18,10 +18,14 @@ def find_port():
     Uno = "0043"
     Red = "6015"
     Leo = "0005"
+    FTDI = "6014"
+    FTDI_C = "6001"
+
 
     #look for ports attached
     tmp = os.popen('python' ' -m' ' serial.tools.list_ports' ' -v').read()
-
+    print "Port Search complete"
+    print tmp
     #serach for typer of Arduino and captures its port
     if Mega in tmp[:]:
         portUSB = tmp.split('\n', 1)
@@ -31,7 +35,7 @@ def find_port():
         port = port.strip()
         return port
 
-    if Uno in tmp[:]:
+    elif Uno in tmp[:]:
         portUSB = tmp.split('\n', 1)
         print "Device is an Uno"
         print "It is attached to: " + portUSB[0]
@@ -39,22 +43,39 @@ def find_port():
         port = port.strip()
         return port
 
-    if Red in tmp[:]:
+    elif Red in tmp[:]:
         portUSB = tmp.split('\n', 1)
         print "Device is a Redboard"
-        print "It is attached to:"
         print "It is attached to: " + portUSB[0]
+        port = str(portUSB[0])
         port = port.strip()
         return port
 
-    if Leo in tmp[:]:
+    elif Leo in tmp[:]:
         portUSB = tmp.split('\n', 1)
         print "Device is a Leonardo"
-        print "It is attached to:"
         print "It is attached to: " + portUSB[0]
+        port = str(portUSB[0])
         port = port.strip()
         return port
-
+    elif FTDI in tmp[:]:
+        portUSB = tmp.split('\n', 4)
+        print "Device is a using a FTDI cable"
+        print "It is attached to: " + portUSB[3]
+        port = str(portUSB[3])
+        port = port.strip()
+        return port
+    elif FTDI_C in tmp[:]:
+        portUSB = tmp.split('\n', 4)
+        print "Device is a using a USB FTDI Convertor"
+        print "It is attached to: " + portUSB[3]
+        port = str(portUSB[3])
+        port = port.strip()
+        return port
+    elif Mega and Uno and Red and Leo and FTDI not in tmp[:]:
+        print "no active port"
+        port = "none"
+        return port
 
 #assigns port from function
 port = str(find_port())
